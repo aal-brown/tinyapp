05542e4e1aa9eb7188f194829650994e6473ba4c
aal-brown <adlai.brown@gmail.com>
2020-01-20 23:59:20 +0000

    Added a body parser and a random string generator.

--- express_server.js
+++ express_server.js
@@ -10,6 +10,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//toString(36) means to use any numbers from 0 to 9 and any letters from a to z. So 26+10 = 36
+function generateRandomString() {
+  return Math.random().toString(36).substr(2,6)
+} ;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!"); // ends the request-response loop and gives a message.
 });
@@ -27,10 +35,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars); //assumed .ejs extension, thus EJS knows to look in the "views" folder by default.
 });
 
+//This needs to be above the :shortURL because otherwise any calls to urls new will be handled by the :shortURL.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
