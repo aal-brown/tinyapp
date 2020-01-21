2369751c511c50aa9875f18f6dbea2621d002f0e
aal-brown <adlai.brown@gmail.com>
2020-01-21 16:58:18 +0000

    Added a delete button to the urls page for each url, added a post method to the express server, however it isn't functional yet.

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ const urlDatabase = {
 //toString(36) means to use any numbers from 0 to 9 and any letters from a to z. So 26+10 = 36
 function generateRandomString() {
   return Math.random().toString(36).substr(2,6)
-} ;
+};
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -46,11 +46,24 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body["longURL"];
+  //console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params.shortURL);
+  delete urlDatabase[req.params.shortURL];
+  //console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls_index`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
