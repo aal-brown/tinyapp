7db1dd3e367dd4e959d394909e0aeea0334b16f4
aal-brown <adlai.brown@gmail.com>
2020-01-22 23:48:28 +0000

    Fully implemented register, login, logout.

--- express_server.js
+++ express_server.js
@@ -14,30 +14,32 @@ const urlDatabase = {
 
 const users = {
 
-  addUser: function (reqBody) {
+  addUser: function(reqBody) {
     let newID = generateRandomString();
     users[newID] = {
       id: newID,
-      email: reqBody.body.email,
-      password: reqBody.body.password,
+      email: reqBody.email,
+      password: reqBody.password,
     };
+    return newID;
+  },
+  
+  //If the email is found, it returns the user id, otherwise it returns undefined
+  getIDfromEmail: function(email) {
+    for (let ids in users) {
+      if (users[ids].email === email) {
+        console.log(users[ids].id);
+        return users[ids].id;
+      }
+    }
   }
 };
-
-class Users {
-  constructor(userID, email, password) {
-    this.id = userID;
-    this.email = email;
-    this.password = password;
-  }
-}
-
 
 
 
 //toString(36) means to use any numbers from 0 to 9 and any letters from a to z. So 26+10 = 36
-function generateRandomString() {
-  return Math.random().toString(36).substr(2,6)
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2,6);
 };
 
 const bodyParser = require("body-parser");
@@ -56,26 +58,40 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { username: req.cookies["username"],
-    urls: urlDatabase };
+  let userID = req.cookies["user_id"]
+  let templateVars = {
+    userID : users[userID],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars); //assumed .ejs extension, thus EJS knows to look in the "views" folder by default.
 });
 
 //This needs to be above the :shortURL because otherwise any calls to urls new will be handled by the :shortURL.
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let userID = req.cookies["user_id"];
+  let templateVars = {
+    userID : users[userID]
+  };
   res.render("urls_new", templateVars);
 });
 
 //Goes to the registration page, has to be above the :shortURL because otherwise "registration" will be treated as the new url on the urls_show page.
 app.get("/register", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
-  res.render("urls_registration", templateVars);
+  let userID = req.cookies["user_id"];
+  let templateVars = {
+    userID : users[userID]
+  };
+  res.render("register", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { username: req.cookies["username"],
-    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let userID = req.cookies["user_id"];
+  let templateVars = {
+    userID : users[userID],
+    shortURL: req.params.shortURL,
+    longURL:req.body.longURL
+  };
+
   res.render("urls_show", templateVars);
 });
 //The render command is basically saying, that when we make a get request for the shortURL, we will respond by sending a rendered urls_show file.
@@ -83,7 +99,6 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body["longURL"];
-  //console.log(req.body);  // Log the POST request body to the console
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -94,47 +109,70 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  //console.log(req.body);  // Log the POST request body to the console
   res.redirect(`/urls`);
 });
 
 //This code is used for edit requests from the main urls page.
 app.post("/urls/:shortURL", (req, res) => {
-  let templateVars = { username: req.cookies["username"],
-    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let userID = req.cookies["user_id"];
+  let templateVars = {
+    userID : users[userID],
+    shortURL: req.params.shortURL,
+    longURL:req.body.longURL
+  };
   res.render("urls_show", templateVars);
 });
 
 //This is used for the edit requests at the urls_show page
 app.post("/urls/:shortURL/edit", (req, res) => {
-  let templateVars = { username: req.cookies["username"],
-    shortURL: req.params.shortURL, longURL: req.body.longURL};
+  let userID = req.cookies["user_id"];
+  let templateVars = {
+    userID : users[userID],
+    shortURL: req.params.shortURL,
+    longURL:req.body.longURL
+  };
   urlDatabase[req.params.shortURL] = req.body.longURL;
-  console.log(templateVars)
   res.render("urls_show", templateVars);
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("username",req.body.username);
-  // let templateVars = {
-  //   username: req.cookies["username"],
-  //   urls: urlDatabase
-  // };
-  res.redirect("/urls");
+  let userID = users.getIDfromEmail(req.body.email);
+  if (!req.body.email || !req.body.password) {
+    res.send("400 Invalid entry.");
+  } else if (userID === undefined) {
+    res.send("403 Account doesn't exist.");
+  } else if (users[userID].password !== req.body.password) {
+    res.send("403 Incorrect email address or password.");
+  } else {
+    res.cookie("user_id", userID);
+    res.redirect("/urls");
+  }
 });
 
 //This handles the logout request, it clears the cookie of the username and redirects back to the main page.
 app.post("/logout", (req, res) => {
-  res.clearCookie("username", req.body.username);
+  res.clearCookie("user_id", req.cookies["user_id"]);
   res.redirect("/urls");
 });
 
 app.post("/register", (req, res) => {
-  users.addUser(req.body)
-  console.log(users);
-  res.redirect("/urls");
+  if (!req.body.email || !req.body.password) {
+    res.send("400 Invalid entry.");
+  } else if (users.getIDfromEmail(req.body.email)) {
+    res.send("400 Email already in use.");
+  } else {
+    res.cookie("user_id",users.addUser(req.body));
+    res.redirect("/urls");
+  }
 });
 
+app.get("/login", (req, res) => {
+  let userID = req.cookies["user_id"];
+  let templateVars = {
+    userID : users[userID],
+  };
+  res.render("login", templateVars);
+});
 
 
 app.listen(PORT, () => {
